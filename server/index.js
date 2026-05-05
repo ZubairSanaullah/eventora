973b18b063be18f4f319d4fc277d86f61df57405
Zubair <zubairsanaullah1122@gmail.com>
2026-05-05 14:26:05 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+// Connect to MongoDB
+const uri = process.env.MONGODB_URI;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
